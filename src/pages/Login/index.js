7d0d099f97bb28da40126bcfa672ca3144c16d5c
Bruno Santos <brunorobertocezarsantos@gmail.com>
2022-07-11 10:31:38 -0300

    Tela de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './login.css';
-import logo from '../../assets/carrinho.jpg';
+import logo from '../../assets/carrinho.png';
 
 function Login(){
     const [email, setEmail] = useState('')
@@ -9,12 +9,12 @@ function Login(){
     return(
         <div className='container-center'>
             <div className='login'>
-                <div className='logo-area'>
-                    <img src={logo} alt='Siatema Logo'/>
+                <div className='login-area'>
+                    <img src={logo} alt='Sistema Logo'/>
                 </div>
                 <form>
                     <h1>Entrar</h1>
-                    <input type='text' placeholder='email@email.com'/>
+                    <input type='text' placeholder='email@email.com' value={email} onChange={ (e) => setEmail(e.target.value) }/>
                     <input type='password' placeholder='*******'/>
                     <button type='submit'>Acessar</button>
                 </form>
