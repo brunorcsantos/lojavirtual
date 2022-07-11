5096b9fe9b03d778ddb572021aecee7115056f49
Bruno Santos <brunorobertocezarsantos@gmail.com>
2022-07-11 15:38:21 -0300

    att

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,10 +2,18 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './login.css';
 import logo from '../../assets/carrinho.png';
+import GoogleLogin from 'react-google-login';
 
 function Login(){
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const clientId = '427075692444-9ot51bv68ps3k7s3t66mq7d1kr9csppm.apps.googleusercontent.com';
+
+    const responseGoogle = (response) => {
+        console.log(response);
+      }
+
     return(
         <div className='container-center'>
             <div className='login'>
@@ -19,6 +27,12 @@ function Login(){
                     <button type='submit'>Acessar</button>
                 </form>
                 <Link to='/cadastro'>Criar uma conta</Link>
+
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText='Entrar com Google'
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}/>
             </div>
 
         </div>
