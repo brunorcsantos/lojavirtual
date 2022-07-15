1d24f5bea3239125cd47aa2cb6b0c4417e29a485
adriano-create <batistaaguilaradriano@gmail.com>
2022-07-15 16:36:16 -0300

    Sliders implementados

---
+++ src/components/mainpage/Sdata.js
@@ -0,0 +1,28 @@
+const Sdata = [
+    {
+      id: 1,
+      title: "Só na Lobtec Store. 50% Off Em sua primeira compra, aproveite!",
+      desc: "A melhor expêriencia em compras você encontra aqui, vem pra Lobtec Store! Só vem!",
+      cover: "./images/SlideCard/slide-1.png",
+    },
+    {
+      id: 2,
+      title: "Só na Lobtec Store. 50% Off Em sua primeira compra, aproveite!",
+      desc: "A melhor expêriencia em compras você encontra aqui, vem pra Lobtec Store! Só vem!",
+      cover: "./images/SlideCard/slide-2.png",
+    },
+    {
+      id: 3,
+      title: "Só na Lobtec Store. 50% Off Em sua primeira compra, aproveite!",
+      desc: "A melhor expêriencia em compras você encontra aqui, vem pra Lobtec Store! Só vem!",
+      cover: "./images/SlideCard/slide-3.png",
+    },
+    {
+      id: 4,
+      title: "Só na Lobtec Store. 50% Off Em sua primeira compra, aproveite!",
+      desc: "A melhor expêriencia em compras você encontra aqui, vem pra Lobtec Store! Só vem!",
+      cover: "./images/SlideCard/slide-4.png",
+    },
+  ]
+  export default Sdata
+  
