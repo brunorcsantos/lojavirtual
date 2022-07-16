80cbfdf25ca072502a845f2aa656823502d27a9b
Bruno Santos <brunorobertocezarsantos@gmail.com>
2022-07-15 23:06:21 -0300

    Página Produtos finalizada parcialmente

--- src/pages/Produto/index.js
+++
@@ -1,7 +0,0 @@
-function Produto(){
-    return(
-        <h1>Produto Escolhido:</h1>
-    )
-}
-
-export default Produto;
--- src/pages/Produtos/index.js
+++ src/pages/Produtos/index.js
@@ -1,7 +1,61 @@
+import { useEffect, useState } from 'react';
+import  api  from '../../services/api';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
 function Produtos(){
+    const [produtos, setProdutos] = useState([]);
+
+    useEffect(() => {
+        async function getProdutos() {
+            const response = await api.get('/produtos');
+            setProdutos(response.data)
+          }
+          getProdutos();
+    
+      
+    })
+    
+
     return(
-        <h1>Escolha um Produto</h1>
+        <div className='container'>
+            {produtos.map((produto) => {
+                return(
+                    <Card sx={{ maxWidth: 345,
+                    display: 'inline-block',
+                    margin: 4 }} >
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="/static/images/cards/contemplative-reptile.jpg"
+                            alt="green iguana"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                            {produto.nome}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                            {produto.caracteristica}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">{produto.categoria}</Button>
+                            <Button size="small">Saiba mais</Button>
+                        </CardActions>
+                    </Card>
+                )
+            })}
+        </div>
     )
 }
 
 export default Produtos;
+
+
+
