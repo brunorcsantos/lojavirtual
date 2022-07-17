46aae650c4e8c66e2fa9a557d526101687ab3ffc
DouglasProx <proxsider@gmail.com>
2022-07-17 20:10:48 -0300

    Anuncios com imagem do banco 1.1

--- src/pages/Anuncios/CardImage.js
+++ src/pages/Anuncios/CardImage.js
@@ -6,7 +6,8 @@ import CardMedia from '@mui/material/CardMedia';
 
 
 function CardImage({anuncio}){
-    const [imagem, setImagem] = useState([]);
+    const [imagem, setImagem] = useState();
+    let isURL = false;
     
 
 
@@ -15,29 +16,43 @@ function CardImage({anuncio}){
         async function getAnuncios() {
             api.get('/produtos/' + anuncio.produto + '/imagem')
             .then( response => {
-                setImagem("https://images2.alphacoders.com/837/837293.jpg");
                 console.log(response.data)
                 const base64String = btoa(String.fromCharCode(...new Uint8Array(response.data.img.data.data)));
                 setImagem(base64String)
+                if(response.data.nome == 'urlImage'){
+                    isURL = true;
+                }
                 
                 
             })
             .catch(error => {
                 setImagem("https://autolivraria.com.br/bc/wp-content/uploads/2016/07/Chevrolet-Prisma-01.jpg");
+                isURL = true;
             })
           }
           getAnuncios();
     }, [count])
 
     return(
-
+        <>
+        {isURL ?
             <CardMedia
                 component="img"
-                
+                height="140"
+                image= {imagem}
+                alt="green iguana"
+            />
+
+            :
+            <CardMedia
+                component="img"
                 height="140"
                 image= {`data:image/png;base64,${imagem}`}
                 alt="green iguana"
             />
+
+        }
+        </>
     )
 }
 
